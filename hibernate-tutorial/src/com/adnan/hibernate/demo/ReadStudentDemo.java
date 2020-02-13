e63ddd8fe727a0cf0a5740937d3ea186df855a40
nassar <nassar@DE-ADN-1N7FKV1.adesso.local>
2020-02-13 17:56:30 +0100

    Updating data in database using hibernate

--- hibernate-tutorial/src/com/adnan/hibernate/demo/ReadStudentDemo.java
+++ hibernate-tutorial/src/com/adnan/hibernate/demo/ReadStudentDemo.java
@@ -1,28 +1,45 @@
 package com.adnan.hibernate.demo;
 
-import java.nio.channels.SeekableByteChannel;
-
+import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import com.adnan.hibernate.demo.entity.Student;
 
 public class ReadStudentDemo {
+	static SessionFactory factory;
 
 	public static void main(String[] args) {
+
 		Student s1 = getStudentByID(1);
-		if(s1!=null) {
+		if (s1 != null) {
 			System.out.println("Retvied Object : " + s1);
 		}
+
+		// updating the object with the id "2"
+		if (updateStudentsLastName(2, "Nassaro") == true) {
+
+			List<Student> theStudents = getAllStudents();
+			if (theStudents != null) {
+				theStudents.forEach(a -> System.out.println(a));
+			}
+		}
+
 	}
 
-	static Student getStudentByID(int id) {
+	static Session createSession() {
 		// create session factory
-		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class)
+		factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class)
 				.buildSessionFactory();
 		// create session
 		Session session = factory.getCurrentSession();
+		if (session != null)
+			return session;
+		return null;
+	}
 
+	static Student getStudentByID(int id) {
+		Session session = createSession();
 		try {
 			// use the session to read object from database
 
@@ -50,5 +67,62 @@ public class ReadStudentDemo {
 
 		return null;
 
+	}
+
+	static List<Student> getAllStudents() {
+		Session session = createSession();
+		try {
+			// use the session to read object from database
+
+			// find the student's id "primary Key"
+			// but first we need to start a transaction
+			session.beginTransaction();
+			// retrive all students in the data base
+			// List<Student> theStudents = session.createQuery("from
+			// Student").getResultList();
+
+			// retrive students from the data base with some conditions
+			List<Student> theStudents = session
+					.createQuery("from Student s where s.lastName = 'Nassar'" + " or s.firstName = 'Yasin'")
+					.getResultList();
+
+			// commit the transaction
+			session.getTransaction().commit();
+			if (theStudents != null) {
+				System.out.println("DONE!");
+				return theStudents;
+			}
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			factory.close();
+		}
+
+		return null;
+	}
+
+	static boolean updateStudentsLastName(int id, String lastName) {
+		Session session = createSession();
+		if (session != null) {
+			try {
+				// starting the session
+				session.beginTransaction();
+
+				// 1 : retrieving the object then edit it before save it again
+				getStudentByID(id).setLastName(lastName);
+				session.getTransaction().commit();
+
+				return true;
+
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			} finally {
+				factory.close();
+			}
+		} else {
+			return false;
+		}
+		return false;
 	}
 }
